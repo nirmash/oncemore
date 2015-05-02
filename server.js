9c5e1624044ee4cae49d0037a00dcc4fa985cfd1
nirma <nirmsk@hotmail.com>
2015-05-01 17:15:08 -0700

    two

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Code dd and Azure Web App44</h1>');
+    res.end('<h1>Hello Code and Azure Web App</h1>');
 }).listen(process.env.PORT);  
 
 //git remote add origin https://github.com/nirmash/
